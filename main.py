7dfa3b6ae4816b08b63dade54f45d552fd464425
CosmicMashups <129129115+CosmicMashups@users.noreply.github.com>
2024-06-21 21:19:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils import delay, read_database, bpm_finder, key_finder, sort_bpm, sort_keys
+from match import delay, read_database, song_match      # bpm_finder, key_finder, sort_bpm, sort_keys
+from search import search_song                          # sorting
 
 div = "=" * 15
 print(f"{div} WELCOME TO MIXSYNC {div}")
@@ -8,12 +9,14 @@ seamless music-matching experience.""")
 
 while True:
     surrounding_keys = []
-    db_opt = [1, 2]
+    db_opt = [1, 2, 3, 4]
+    process_opt = [1, 2, 3, 4]
 
     while True:
-        print(f"{div}===================={div}")
-        print("""[1] ANIME      [2] WESTERN""")
-        db_name = int(input("Choose the type of song you want to find from [#]: "))
+        print(f"{div} SONG TYPE {div}")
+        print("""[1] ANIME      [3] K-POP""")
+        print("""[2] WESTERN    [4] OPM""")
+        db_name = int(input("Select the song type you wish to search from [#]: "))
         if db_name in db_opt:
             break
         else:
@@ -30,6 +33,12 @@ while True:
             db_name = 'western.csv'
             # List of keys
             key_list = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+        case 3:
+            print("We're sorry. The database containing K-Pop songs is still not ready yet.")
+            break
+        case 4:
+            print("We're sorry. The database containing OPM songs is still not ready yet.")
+            break
 
     try:
         database, columns_to_display = read_database(db_name)
@@ -37,62 +46,65 @@ while True:
         print(e)
         exit()
 
-    # User input: BPM, Key, BPM and Key Threshold
     while True:
-        try:
-            bpm = int(input("Specify the BPM (#): "))
-            if bpm not in range(70, 140):
-                raise ValueError("BPM must be between 70 and 139.")
-            break
-        except ValueError:
-            print("Input is not a valid number or outside the range.")
-
-    while True:
-        key = input("Specify the Key: ")
-        if key in key_list:
+        print(f"{div} PROCESS {div}")
+        print("""[1] SEARCH             [3] MATCH (BY KEY, BPM)""")
+        print("""[2] ADD SONGS          [4] MATCH (BY SONG)""")
+        process = int(input("Which procedure would you like to perform [#]: "))
+        if process in process_opt:
             break
         else:
-            print("Input is not a valid key.")
+            print("Invalid option.")
+            continue
 
-    while True:
-        try:
-            bpm_thresh = int(input("Specify the BPM Gap (#): "))
+    match process:
+        # SEARCH
+        case 1:
+            searched = search_song(database, db_name, columns_to_display, div)
+
+            if searched is not None and not searched.empty:
+                searched_title = searched.iloc[0]['TITLE']
+                search_match = input(f"Do you want to search songs that would match '{searched.iloc[0]['TITLE']}' by '{searched.iloc[0]['ARTIST']}' [Y/N]: ").lower()
+
+                if search_match == "y":
+                    bpm = int(searched.iloc[0]['BPM'])
+                    key = searched.iloc[0]['KEY']
+                    song_match(database, bpm, key, key_list, columns_to_display, div)
+                else:
+                    print("No search performed.")
+
+            else:
+                print("No search performed.")
+
+        # ADD SONGS
+        case 2:
+            print("We're sorry. The feature of adding songs is still not available.")
             break
-        except ValueError:
-            print("Input is not a valid number.")
 
-    while True:
-        try:
-            key_thresh = int(input("Specify the Key Gap (#): "))
+        # MATCH (BY KEY, BPM)
+        case 3:
+            while True:
+                try:
+                    bpm = int(input("Specify the BPM (#): "))
+                    if bpm not in range(70, 140):
+                        raise ValueError("BPM must be between 70 and 139.")
+                    break
+                except ValueError:
+                    print("Input is not a valid number or outside the range.")
+
+            while True:
+                key = input("Specify the Key: ")
+                if key in key_list:
+                    break
+                else:
+                    print("Input is not a valid key.")
+
+            song_match(database, bpm, key, key_list, columns_to_display, div)
+
+        # MATCH (BY SONG)
+        case 4:
+            print("We're sorry. The feature of matching songs by a specific song is still not available.")
             break
-        except ValueError:
-            print("Input is not a valid number.")
-
-    # Adjust bpm and bpm_thresh if bpm is not in the range
-    if bpm not in range(70, 140):
-        bpm /= 2
-        bpm_thresh /= 2
-
-    sort_db = sort_bpm(database, bpm, bpm_thresh)
-
-    # # User input: Ask if the list needs to be sorted
-    # sort_option = input("Do you want to sort the list (Yes/No): ").lower()
-    # if sort_option == "yes":
-    #     sort_column = input(f"How do you want to sort it ({', '.join(columns_to_display)}): ")
-    #     sort_db = sorting(sort_column)
-    # else:
-    #     sort_db = database  # Use the unsorted database
-
-    # Function calls: Find songs
-    find_bpm = bpm_finder(sort_db, bpm, bpm_thresh)
-    find_keybpm = key_finder(find_bpm, key, key_thresh, key_list)
-    sorted_keybpm = sort_keys(find_keybpm, key, key_thresh, key_list)
-
-    # Display results
-    delay()
-    print(f"{div} SONGS {div}")
-    print(sorted_keybpm[columns_to_display])
-    print(f"{div}======={div}")
 
     # User input: Ask if the user wants to run another query
     option = input("Do you still want to search (Yes/No): ").lower()
